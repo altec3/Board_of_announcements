3ee8d5e0bbf6d91559efb5a7893e7286791ae373
Aleksey Lesnikov <poprobuyu@mail.ru>
2022-12-06 10:15:49 +0500

    Modified urls.py

--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
